36f552a8d9a13d6aba93806b48275622fd0b843d
Piyush Singh <1806397@kiit.ac.in>
2023-09-07 16:56:55 +0530

    Tailwind:flex

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,4 +1,4 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Piyush</div>;
+  return <div className="hidden text-sm font-semibold md:block">Piyush</div>;
 }
 export default Username;
