3a62ea82265861f42b241e4a37a847869d01bf55
Piyush Singh <1806397@kiit.ac.in>
2023-09-07 18:39:51 +0530

    Tailwind:Styling button

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,30 +1,30 @@
-import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
-import { createOrder } from "../../services/apiRestaurant";
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
+import { createOrder } from '../../services/apiRestaurant';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: "Mediterranean",
+    name: 'Mediterranean',
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: "Vegetale",
+    name: 'Vegetale',
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: "Spinach and Mushroom",
+    name: 'Spinach and Mushroom',
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -33,7 +33,7 @@ const fakeCart = [
 
 function CreateOrder() {
   const navigation = useNavigation();
-  const isSubmitting = navigation.state === "submitting";
+  const isSubmitting = navigation.state === 'submitting';
   const formErrors = useActionData();
   const cart = fakeCart;
 
@@ -45,7 +45,9 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <div>
+            <input type="text" name="customer" required />
+          </div>
         </div>
 
         <div>
@@ -71,13 +73,21 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority">Want to give your order priority?</label>
         </div>
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
-            {isSubmitting ? "Placing Order ..." : "Order now"}
+          <button
+            disabled={isSubmitting}
+            className="focus:ring-yellow mt-2 inline-block
+             rounded-full bg-yellow-400 px-4 py-3 font-semibold 
+             uppercase tracking-wide text-stone-800 
+             transition-colors duration-300 hover:bg-yellow-300 
+             focus:outline-none focus:ring focus:ring-yellow-300 
+             focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-slate-600"
+          >
+            {isSubmitting ? 'Placing Order ...' : 'Order now'}
           </button>
         </div>
       </Form>
@@ -90,12 +100,12 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === "on",
+    priority: data.priority === 'on',
   };
   const errors = {};
   if (!isValidPhone(order.phone))
     errors.phone =
-      "Please give us your correct phone number. We might need it to contact you";
+      'Please give us your correct phone number. We might need it to contact you';
   if (Object.keys(errors).length > 0) return errors;
   // If everything is OK, return the redired data
   const newOrder = await createOrder(order);
